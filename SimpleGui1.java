0ed1715f1d476a2f136ae4721c3e8f386b4b77f6
Исмаилов Саид <mazgOFF.net@mail.ru>
2022-08-19 12:31:22 +0300

    fix: реализована обработка событий при нажатии на кнопку

--- SimpleGui1.java
+++ SimpleGui1.java
@@ -1,16 +1,29 @@
 import javax.swing.*;
+import java.awt.event.*; // здесь хранятся ActionListener и ActionEvent
 
-public class SimpleGui1
+public class SimpleGui1 implements ActionListener
 {
+    JButton button;
+
     public static void main(String[] args)
     {
+        SimpleGui1 gui = new SimpleGui1();
+        gui.go();
+    }
+    public void go()
+    {
         JFrame frame = new JFrame();
-        JButton button = new JButton("CLICK ME");
+        button = new JButton("CLICK ME");
 
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // завершение программы при закрытии окна
+        button.addActionListener(this); // аргумент ДОЛЖЕН реализовывать Action Listener
 
         frame.getContentPane().add(button); // добавляем кнопку на панель фрейма
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // завершение программы при закрытии окна
         frame.setSize(300, 300); // размер фрейма в пикселях
         frame.setVisible(true); // делаем фрейм видимым
     }
+        public void actionPerformed(ActionEvent event)
+        {
+            button.setText("I'VE BEEN CLICKED");
+        }
 }

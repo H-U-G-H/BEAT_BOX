27b00f3d1afbbba2680fb5c105e388d58b1e6ec3
Исмаилов Саид <mazgOFF.net@mail.ru>
2022-09-03 10:34:09 +0300

    feat: изучены особенности работы с JTextArea и JScrollPane.

---
+++ TextAreaTest.java
@@ -0,0 +1,41 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TextAreaTest implements ActionListener
+{
+    JTextArea area;
+    public static void main(String[] args)
+    {
+        TextAreaTest gui = new TextAreaTest();
+        gui.go();
+    }
+    
+    public void go()
+    {
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(250, 250);
+        frame.setVisible(true);
+
+        JPanel panel = new JPanel();
+        JButton button = new JButton("Just click it");
+        button.addActionListener(this);
+        area = new JTextArea(10, 20);
+        area.setLineWrap(true); // ПЕРЕНОС СТРОК (TRUE)
+
+        JScrollPane scroller = new JScrollPane(area);
+        scroller.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scroller.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+
+        panel.add(scroller);
+        frame.getContentPane().add(BorderLayout.CENTER, panel);
+        frame.getContentPane().add(BorderLayout.SOUTH, button);
+    } // OUT OF GO CLASS
+
+    public void actionPerformed(ActionEvent event)
+    {
+        area.append("button clicked \n");
+    }
+}
